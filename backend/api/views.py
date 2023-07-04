a69e5076fe215f2b55d0b1d6e28a5150d97662b0
Александр Савельев <savelyevaleksandr@yandex.ru>
2023-07-05 01:50:03 +0400

    fourth review

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
