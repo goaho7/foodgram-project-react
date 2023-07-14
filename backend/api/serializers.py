624b912d329f76892f9b2b0a15ca2476be1d35ba
Александр Савельев <savelyevaleksandr@yandex.ru>
2023-07-14 15:48:16 +0400

    test

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,7 +8,6 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from django.shortcuts import get_object_or_404
-
 from recipes.models import (
     Favorite, Ingredient, IngredientAmount, Recipe,
     ShoppingCart, Tag
