2166067a70cebfae9242bc082eada62cfdb76a89
Александр Савельев <savelyevaleksandr@yandex.ru>
2023-07-14 16:53:57 +0400

    test

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.core.files.base import ContentFile
 from django.db.transaction import atomic
 from djoser.serializers import UserCreateSerializer, UserSerializer
-#from drf_extra_fields.fields import Base64ImageField
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from django.shortcuts import get_object_or_404
@@ -141,16 +141,16 @@ class IngredientAmountSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'measurement_unit', 'amount',)
 
 
-class Base64ImageField(serializers.ImageField):
-    """ Класс для работы с изображениями """
+# class Base64ImageField(serializers.ImageField):
+#     """ Класс для работы с изображениями """
 
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+#     def to_internal_value(self, data):
+#         if isinstance(data, str) and data.startswith('data:image'):
+#             format, imgstr = data.split(';base64,')
+#             ext = format.split('/')[-1]
+#             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
 
-        return super().to_internal_value(data)
+#         return super().to_internal_value(data)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -300,7 +300,7 @@ class AddRecipeSerializer(serializers.ModelSerializer):
 
     @atomic
     def update(self, instance, validated_data):
-        ingredients = validated_data.pop('ingredients')
+        ingredients = validated_data.pop('ingredients_in_recipe')
         tags = validated_data.pop('tags')
 
         for key in validated_data:
